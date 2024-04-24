7d609a00543e9966981202ad4a987017947b5c83
aj <aricsjones@gmail.com>
2024-04-23 22:28:14 -0400

    Initial Commit

---
+++ TestingArea/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestingArea
+//
+//  Created by A J on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingArea/TestingAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAreaApp.swift
+//  TestingArea
+//
+//  Created by A J on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
